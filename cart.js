6b25ac74f2bda0a08808992007ac054d0c21e163
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-06-03 11:16:10 +0600

    local storage interactions for a shopping cart

--- cart.js
+++ cart.js
@@ -9,6 +9,7 @@ const addProduct = () =>{
     
     console.log(product, quantity);
     displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
 }
 
 const displayProduct = (product, quantity) =>{
@@ -17,3 +18,31 @@ const displayProduct = (product, quantity) =>{
     li.innerText = `${product}: ${quantity}`
     ul.appendChild(li);
 }
+
+const getStoredShoppingCart = () =>{
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) =>{
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified)
+}
+
+const displayProductsFromLocalStorage = () =>{
+    const savedCart = getStoredShoppingCart();
+    console.log(savedCart);
+    for(const product in savedCart){
+        const quantity = savedCart[product];
+        console.log(product, quantity);
+        displayProduct(product, quantity);
+    }
+}
+
+displayProductsFromLocalStorage();
